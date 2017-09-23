7b7ca0275c655206e2c782c0c5248335d82ea5e1
sushmasanisetty <sushmasanisetty@gmail.com>
2017-09-23 16:07:31 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-1', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'sushu.html'));
+  res.send('grrr');
 });
 
 
